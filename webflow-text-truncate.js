4a5030166cc9d06cbcf75ef000efcbf295c36f27
Ahmed Qayyum <ahmedqayyum32@gmail.com>
2022-11-19 15:59:15 +0500

    Update webflow-text-truncate.js

--- webflow-text-truncate.js
+++ webflow-text-truncate.js
@@ -1,12 +1,25 @@
 <script>
+// 🍉 when DOM has loaded
 document.addEventListener("DOMContentLoaded", () => {
-  const maxCharacters = 1400, // whatever max charachter you like to display
-    targets = document.getElementsByClassName("about-rector");
-
-  Array.from(targets).forEach((target) => {
-    const text = target.textContent,
-      truncate = text.substring(0, maxCharacters);
-    target.textContent = `${truncate} ...`;
-  });
+  const limit = 130, // number of character allowed before truncation trigger
+    maxCharacters = 130; // number of characher displayed after elipsis
+  truncateParagraphs(limit, maxCharacters);
 });
+
+// 🍆 helper function
+function countCharacters(target) {
+  const characters = target.textContent.split("");
+  return characters.length; // returns the amount of characters incl. spaces
+}
+
+// 🥬 truncate function
+function truncateParagraphs(limit, maxCharacters) {
+  const targets = document.getElementsByClassName("heading-style-h5 news-truncate");
+  Array.from(targets).forEach((target) => {
+    if (countCharacters(target) < limit) return; // skip truncation
+    const text = target.textContent,
+      elipsis = text.substring(0, maxCharacters);
+    target.textContent = `${elipsis}...`;
+  }); // end for each
+}
 </script>
