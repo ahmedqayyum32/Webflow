51dc6eaf1d3eb3b8e2e7e0ba9c426dfec16bd020
Ahmed Qayyum <ahmedqayyum32@gmail.com>
2022-12-07 20:40:33 +0500

    Update time-passed.js

--- time-passed.js
+++ time-passed.js
@@ -1,39 +1,47 @@
 <script>
   // Function to pluralize the time past (eg. minute or minutes / day or days)
-  const pluralize = (count, noun, suffix = 's') => `${count} ${noun}${count !== 1 ? suffix : ''}`;
+const pluralize = (count, noun, suffix = 's') => `${count} ${noun}${count !== 1 ? suffix : ''}`;
 
-  const timePast = (curr, prev) => {
+//Time passed calculation
+const timePast = (curr, prev) => {
     const msMin = 60 * 1000, msHr = msMin * 60, msDay = msHr * 24, msMonth = msDay * 30, msYr = msDay * 365;
     let elapsed = curr - prev;
 
-    if (elapsed < msMin) return pluralize(Math.round(elapsed/1000), 'second');
+    if (elapsed < msMin) return pluralize(Math.round(elapsed / 1000), 'second');
     else if (elapsed < msHr) {
-      elapsed = Math.round(elapsed/msMin);
-      return pluralize(elapsed, 'minute') 
+        elapsed = Math.round(elapsed / msMin);
+        return pluralize(elapsed, 'minute')
     }
     else if (elapsed < msDay) {
-      elapsed = Math.round(elapsed/msHr);
-      return pluralize(elapsed, 'hour')
+        elapsed = Math.round(elapsed / msHr);
+        return pluralize(elapsed, 'hour')
     }
     else if (elapsed < msMonth) {
-      elapsed = Math.round(elapsed/msDay);
-      return pluralize(elapsed, 'day') 
+        elapsed = Math.round(elapsed / msDay);
+        return pluralize(elapsed, 'day')
     }
     else if (elapsed < msYr) {
-      elapsed = Math.round(elapsed/msMonth);
-      return pluralize(elapsed, 'month') 
+        elapsed = Math.round(elapsed / msMonth);
+        return pluralize(elapsed, 'month')
     }
     else {
-      elapsed = Math.round(elapsed/msYr);
-      return pluralize(elapsed, 'year') 
+        elapsed = Math.round(elapsed / msYr);
+        return pluralize(elapsed, 'year')
     }
-  }
+} //timePast function end
 
-  let now = new Date();
+var d = new Date();
+var n = d.getTimezoneOffset();
+var ans = new Date(d.getTime() + n * 60 * 1000);
 
-// Replace .post-time class
-  document.querySelectorAll('.post-time').forEach(box => {
-    let parsedTime  = Date.parse(box.innerText);
-    box.innerText = timePast(now, new Date(parsedTime)) + ' ago';
-  })
+
+document.querySelectorAll('.post-time').forEach(box => {
+    let createdTimeString = box.innerText + " GMT-05"
+		let created = new Date(createdTimeString)
+		let offset = d.getTimezoneOffset();
+		let createdUTC = new Date(created.getTime() + offset * 60 * 1000);
+
+		box.innerText = timePast(ans, createdUTC) + ' ago';
+})
+
 </script>
