333a0418404c78e33366e4b04c6fcbb0c368790c
Ahmed Qayyum <ahmedqayyum32@gmail.com>
2023-09-17 22:21:47 +0500

    Code updated with attributes

--- reviewcount.js
+++ reviewcount.js
@@ -1,42 +1,51 @@
-document.addEventListener('DOMContentLoaded', function () {
-  // Function to count CMS items
-  function countCMSItems(collectionListClassName) {
-    const collectionList = document.querySelector(`.${collectionListClassName}`);
-    const itemCount = collectionList.querySelectorAll('.w-dyn-item').length;
-    return itemCount;
-  }
+<script>
+  function initializeReviewOverview(element) {
+    const cmsListSelector = element.getAttribute('zd-cms-list');
+    const ratingFieldSelector = element.getAttribute('zd-rating-field');
+    const averageFieldSelector = element.getAttribute('zd-average-field');
+    const totalFieldSelector = element.getAttribute('zd-total-field');
 
-  // Function to calculate the average rating
-  function calculateAverageRating(collectionListClassName) {
-    const collectionList = document.querySelector(`.${collectionListClassName}`);
-    const ratingElements = collectionList.querySelectorAll('.rating-text');
-    let totalRating = 0;
-
-    ratingElements.forEach((ratingElement) => {
-      const rating = parseFloat(ratingElement.textContent);
-      if (!isNaN(rating)) {
-        totalRating += rating;
-      }
-    });
-
-    if (ratingElements.length > 0) {
-      return totalRating / ratingElements.length;
-    } else {
-      return 0; // Default to 0 if there are no ratings
+    // Function to count CMS items
+    function countCMSItems(collectionListSelector) {
+      const collectionList = document.querySelector(collectionListSelector);
+      const itemCount = collectionList.querySelectorAll('.w-dyn-item').length;
+      return itemCount;
     }
+
+    // Function to calculate the average rating
+    function calculateAverageRating(collectionListSelector, ratingSelector) {
+      const collectionList = document.querySelector(collectionListSelector);
+      const ratingElements = collectionList.querySelectorAll(ratingSelector);
+      let totalRating = 0;
+
+      ratingElements.forEach((ratingElement) => {
+        const rating = parseFloat(ratingElement.textContent);
+        if (!isNaN(rating)) {
+          totalRating += rating;
+        }
+      });
+
+      if (ratingElements.length > 0) {
+        return totalRating / ratingElements.length;
+      } else {
+        return 0; // Default to 0 if there are no ratings
+      }
+    }
+
+    const totalElement = document.querySelector(totalFieldSelector);
+    const averageElement = document.querySelector(averageFieldSelector);
+
+    const itemCount = countCMSItems(cmsListSelector);
+    const averageRating = calculateAverageRating(cmsListSelector, ratingFieldSelector);
+
+    totalElement.textContent = `Total Reviews: ${itemCount}`;
+    averageElement.textContent = `Average Rating: ${averageRating.toFixed(2)}`;
   }
 
-  // Update elements with the specified data attributes
-  const overviewElements = document.querySelectorAll('.review-overview');
-  overviewElements.forEach((overviewElement) => {
-    const collectionListClassName = overviewElement.dataset.cmsClass;
-    const totalElement = overviewElement.querySelector('.review-overview_total');
-    const averageElement = overviewElement.querySelector('.review-overview_average');
-
-    const itemCount = countCMSItems(collectionListClassName);
-    const averageRating = calculateAverageRating(collectionListClassName);
-
-    totalElement.textContent = `Total Items: ${itemCount}`;
-    averageElement.textContent = `Average Rating: ${averageRating.toFixed(2)}`;
+  document.addEventListener('DOMContentLoaded', function () {
+    const overviewElements = document.querySelectorAll('.review-overview');
+    overviewElements.forEach((element) => {
+      initializeReviewOverview(element);
+    });
   });
-});
+</script>
